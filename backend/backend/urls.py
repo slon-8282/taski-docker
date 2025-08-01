e0b36b930d19a4fccdf2fe2ba506abeff37fc7d8
Aleksey Zotov <slonslon20142014@gmail.com>
2025-08-01 12:20:08 +0300

    11

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin  # type: ignore
 from django.urls import include, path  # type: ignore
 from rest_framework import routers  # type: ignore
-
 from api import views
 
 router = routers.DefaultRouter()
